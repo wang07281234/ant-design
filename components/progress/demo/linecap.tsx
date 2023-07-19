4c7b419f8755098450ea458b68862011fda4a10f
wang07281234 <139969172+wang07281234@users.noreply.github.com>
2023-07-19 11:37:17 +0100

    Update linecap.tsx
    Signed-off-by: wang07281234 <139969172+wang07281234@users.noreply.github.com>

--- components/progress/demo/linecap.tsx
+++ components/progress/demo/linecap.tsx
@@ -3,7 +3,7 @@ import { Progress, Space } from 'antd';
 
 const App: React.FC = () => (
   <>
-    <Progress strokeLinecap="butt" percent={75} />
+    <Progress strokeLinecap="butt" percent={95} />
     <Space wrap>
       <Progress strokeLinecap="butt" type="circle" percent={75} />
       <Progress strokeLinecap="butt" type="dashboard" percent={75} />
