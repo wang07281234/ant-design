21538aa3c49c291e97d5e8c6245a4ecfb4a5df6b
wang07281234 <139969172+wang07281234@users.noreply.github.com>
2023-07-19 11:35:33 +0100

    Update circle.tsx
    Signed-off-by: wang07281234 <139969172+wang07281234@users.noreply.github.com>

--- components/progress/demo/circle.tsx
+++ components/progress/demo/circle.tsx
@@ -3,7 +3,7 @@ import { Progress, Space } from 'antd';
 
 const App: React.FC = () => (
   <Space wrap>
-    <Progress type="circle" percent={75} />
+    <Progress type="circle" percent={89} />
     <Progress type="circle" percent={70} status="exception" />
     <Progress type="circle" percent={100} />
   </Space>
